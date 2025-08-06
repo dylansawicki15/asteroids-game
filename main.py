0fd6b3aae6a20ad6a6004f94a270ba9cf293e6ee
dylansawicki15 <dylansawicki15@gmail.com>
2025-08-06 20:40:43 +0200

    added framerate, to not drain computer resources

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import sys
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
@@ -13,7 +14,8 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-                return
+
+        dt += clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
