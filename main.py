f09a963f6e66c47e050b08c78bc98073332e73d5
dylansawicki15 <dylansawicki15@gmail.com>
2025-08-06 20:54:24 +0200

    added player to the screen, with the circleshape and the triangle classes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame
 from constants import *
 import sys
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
