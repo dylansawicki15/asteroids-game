16b86b4a36faf3d2a0631f92a20bd91a9a03fd0d
dylansawicki15 <dylansawicki15@gmail.com>
2025-08-07 20:43:08 +0200

    the player can move and rotate now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 from constants import *
-import sys
 from player import Player
 
 def main():
@@ -10,15 +9,16 @@ def main():
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        screen.fill((0, 0, 0))
-        player.draw(screen)
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                return
 
-        dt += clock.tick(60) / 1000
+        screen.fill((0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
